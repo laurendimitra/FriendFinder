d5d4c4a31319cd291784bb64d47b48c23845444d
laurendimitra <lauren.mooore@bluetruckstudios.co>
2017-12-30 23:01:09 -0500

    done

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendsArray = [
+	{
+		name: 'Snake',
+		photo: 'https://fthmb.tqn.com/wRAIcmEcmGr13MU8KYXB-EEdIPo=/2134x1408/filters:fill(auto,1)/78399255-56a2bcfe3df78cf77279622d.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+    },
+    {
+		name: 'Bob',
+		photo: 'https://vignette.wikia.nocookie.net/bobsburgerpedia/images/3/3e/Bobbelcher2.png/revision/latest?cb=20130114075133',
+		scores: [
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2'
+		]
+    },
+    {
+		name: 'Ramona Flowers',
+		photo: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTMwMjU2NzY1NV5BMl5BanBnXkFtZTcwMTQ4MTk2Mw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3'
+		]
+    },
+    {
+		name: 'Troy',
+		photo: 'http://junkee.com/wp-content/uploads/2013/03/troy.jpg',
+		scores: [
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4'
+		]
+    },
+    {
+		name: 'Bernie Sanders',
+		photo: 'https://pbs.twimg.com/profile_images/794596124686487552/kqpbolIc.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
